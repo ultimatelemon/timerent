1d0690e94a084163413baa397e60c4e8f1f14f26
ItsJusty <k.terpstra2000@icloud.com>
2024-07-27 14:42:26 +0200

    Mollie test

--- resources/js/pages/ReservationConfirmation.vue
+++ resources/js/pages/ReservationConfirmation.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div class="text-center" v-if="paid">
+      <div class="text-center" v-if="!paid && loading">
+        <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+          <svg class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+          </svg>
+        </div>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">We controleren je betaling <i v-if="loading" class="fa fa-spinner mr-2 animate-spin"></i> </h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">Scherm niet afsluiten.</p>
+      </div>
+      <div class="text-center" v-if="paid && !loading">
         <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
           <svg class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
@@ -10,7 +19,7 @@
         <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Betaling is gelukt 🥳</h1>
         <p class="mt-6 text-base leading-7 text-gray-600">Je kunt dit scherm nu sluiten.</p>
       </div>
-      <div class="text-center" v-else>
+      <div class="text-center" v-if="!paid && !loading">
         <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-red-100">
           <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -30,6 +39,7 @@ export default {
     return {
       reservation_id: this.$route.params.reservation,
       paid: false,
+      loading: true,
     }
   },
 
@@ -38,6 +48,9 @@ export default {
       axios.get('/reservationispaid/' + this.reservation_id)
           .then(response => {
             this.paid = response.data.data;
+          })
+          .finally(() => {
+            this.loading = false;
           })
     }
   },

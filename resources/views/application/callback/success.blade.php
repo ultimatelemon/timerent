a1dba1cfa122db429413ce16bb19891143c06f46
ItsJusty <k.terpstra2000@icloud.com>
2024-09-14 12:44:53 +0200

    Fix return button to Timerent after subscription paid

--- resources/views/application/callback/success.blade.php
+++ resources/views/application/callback/success.blade.php
@@ -15,8 +15,8 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
             </svg>
         </div>
-        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Betaling is gelukt 🥳</h1>
-        <p class="mt-6 text-base leading-7 text-gray-600">Je kunt dit scherm nu sluiten.</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl mb-4 pb-4">Betaling is gelukt 🥳</h1>
+        <a href="/" class="mt-6 text-base leading-7 text-gray-600">Keer terug naar Timerent &rarr;</a>
         {{--        <div class="mt-10 flex items-center justify-center gap-x-6 group">--}}
         {{--            <a href="/" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Terug naar Rijscholenapp</a>--}}
         {{--            <a href="/" class="text-sm font-semibold text-gray-900 group">Inloggen op je rijschool<span aria-hidden="true">&rarr;</span></a>--}}

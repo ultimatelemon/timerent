99c9383ce4fc1035522db1543c7a4301c62cc3c1
ItsJusty <k.terpstra2000@icloud.com>
2024-09-27 10:10:36 +0200

    Prev fix?

--- resources/js/pages/DashboardComponents/SelectorSideNav.vue
+++ resources/js/pages/DashboardComponents/SelectorSideNav.vue
@@ -7,7 +7,7 @@
     <body class="h-full">
     ```
   -->
-  <div v-if="user && current_venue">
+  <div v-if="current_venue">
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog class="relative z-50 lg:hidden" @close="sidebarOpen = false">
         <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
@@ -79,8 +79,8 @@
                         <!--                <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />-->
                         <div class="flex flex-col">
                           <span class="sr-only">Your profile</span>
-                          <span aria-hidden="true">{{ user.user.name }}</span>
-                          <span aria-hidden="true" class="font-light text-xs">{{ user.user.email }}</span>
+                          <span aria-hidden="true">{{ current_user.name }}</span>
+                          <span aria-hidden="true" class="font-light text-xs">{{ current_user.email }}</span>
                         </div>
                         <div>
                           <i>
@@ -148,8 +148,8 @@
                 <!--                <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />-->
                 <div class="flex flex-col">
                   <span class="sr-only">Your profile</span>
-                  <span aria-hidden="true">{{ user.user.name }}</span>
-                  <span aria-hidden="true" class="font-light text-xs">{{ user.user.email }}</span>
+                  <span aria-hidden="true">{{ current_user.name }}</span>
+                  <span aria-hidden="true" class="font-light text-xs">{{ current_user.email }}</span>
                 </div>
                 <div>
                   <i>
@@ -247,36 +247,29 @@ export default {
     //       })
     // },
 
-    fetchUser(venue) {
-      axios.get('/users/current?venue=' + venue)
+    // fetchUser(venue) {
+    //   axios.get('/users/current?venue=' + venue)
+    //       .then(response => {
+    //         this.user = response.data.data.user;
+    //         this.role = response.data.data.role;
+    //         // this.fetchNavigation();
+    //       })
+    //       .catch(e => {
+    //         console.log(e.message)
+    //       })
+    // },
+
+    fetchUser() {
+      axios.get('/users/current')
           .then(response => {
-            this.user = response.data.data.user;
-            this.role = response.data.data.role;
-            // this.fetchNavigation();
-          })
-          .catch(e => {
-            console.log(e.message)
+            this.current_user = response.data.data;
+            this.current_user_role = response.data.role;
+            this.fetchData();
           })
     },
 
     hasCommon(permission, flags) {
       return flags.some(item1 => permission.some(item2 => item1 === item2));
-    },
-
-    fetchNavigation() {
-      this.navigation = [
-        {
-          'type': 'category',
-          'name': 'Algemeen',
-          'permission': [],
-        },
-        {
-          'name': 'Dashboard',
-          'link': {name: 'venues.home', params: {venue: this.$store.state.venue.id}},
-          'icon': HomeIcon,
-          'permission': [],
-        },
-      ]
     },
 
     logout() {
@@ -289,7 +282,7 @@ export default {
 
   mounted() {
     console.log(this.$store.state.venue.id)
-    this.fetchUser(this.$store.state.venue.id);
+    this.fetchUser();
   },
 
   // TODO: Fix user store in Vuex to remove the api call to fetch user.

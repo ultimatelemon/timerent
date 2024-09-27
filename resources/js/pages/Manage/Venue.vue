fd4c5ba01d1be57323f5cbbb7bbeb2f380a01c01
ItsJusty <k.terpstra2000@icloud.com>
2024-09-27 23:22:11 +0200

    Remove debug line

--- resources/js/pages/Manage/Venue.vue
+++ resources/js/pages/Manage/Venue.vue
@@ -54,8 +54,6 @@
           </div>
           <div class="py-6 sm:grid sm:grid-cols-3 sm:gap-2">
             <dt class="text-sm font-medium leading-6 text-gray-900">Huidig abonnement</dt>
-
-            {{venue}}
             <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ venue.plan.name }}</dd>
           </div>
           <div class="py-6 sm:grid sm:grid-cols-3 sm:gap-2">

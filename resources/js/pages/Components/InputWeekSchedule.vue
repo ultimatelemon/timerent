1801990a4ebc02e66fbfccf97ee21d4cfb576105
ItsJusty <k.terpstra2000@icloud.com>
2024-12-12 14:55:53 +0100

    fixed ui bug in template edit

--- resources/js/pages/Components/InputWeekSchedule.vue
+++ resources/js/pages/Components/InputWeekSchedule.vue
@@ -17,11 +17,11 @@
                   v-model="range.to"
                   class="block w-full rounded-md p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
-              <button v-if="j === 0" :class='j !== 0 ? "hidden" : "hidden"' @click="addRangeToDay(template.ranges)" class="btn btn-primary">
+              <button v-if="j === 0" :class='j !== 0 ? "hidden" : "hidden"' @click="addRangeToDay(template.ranges)" class="btn">
                 <component :is="PlusIcon" class="icon-btn"></component>
               </button>
               <button v-else :class='j === 0 ? "invisible" : "visible"' @click="removeRangeFromDay(template.ranges, j)"
-                      class="btn btn-danger">
+                      class="btn">
                 <component :is="XMarkIcon" class="icon-btn-red"></component>
               </button>
             </div>

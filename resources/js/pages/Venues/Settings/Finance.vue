3e49295a8e4f8ceae1bd27949282082485942be4
ItsJusty <k.terpstra2000@icloud.com>
2024-12-19 22:55:23 +0100

    Front end fix

--- resources/js/pages/Venues/Settings/Finance.vue
+++ resources/js/pages/Venues/Settings/Finance.vue
@@ -70,48 +70,6 @@
       </div>
     </div>
 
-    <!--    <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">-->
-    <!--      <div v-if="formData.payment_service_provider !== 'timerent'">-->
-    <!--        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">API key<span-->
-    <!--            v-if="true"-->
-    <!--            class="required-star">*</span> <span class="text-xs dark:text-slate-400">(Je API key is vanwege veiligheid niet meer zichtbaar na het opslaan van je instellingen)</span></label>-->
-    <!--        <div class="relative mt-2 rounded-md shadow-sm">-->
-    <!--          <input v-model="formData.payment_api_key" type="text" name="name" id="name"-->
-    <!--                 v-on:keyup.enter="postData"-->
-    <!--                 :class="errors.name ? 'ring-red-300' : ''"-->
-    <!--                 class="block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 input"-->
-    <!--                 aria-invalid="true" aria-describedby="name-error"/>-->
-    <!--          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">-->
-    <!--            <svg v-if="errors.name" class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor"-->
-    <!--                 aria-hidden="true">-->
-    <!--              <path fill-rule="evenodd"-->
-    <!--                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"-->
-    <!--                    clip-rule="evenodd"/>-->
-    <!--            </svg>-->
-    <!--          </div>-->
-    <!--        </div>-->
-    <!--      </div>-->
-    <!--      <div v-else-if="!venue.stripe_connect_id || !venue.stripe_connect_onboarded">-->
-    <!--        &lt;!&ndash;        <div v-if="venue.stripe_connect_id && venue.stripe_connect_onboarded" class="text-green-500"><i class="fa fa-check"></i> Je kunt gebruik maken van Timerent Payments</div>&ndash;&gt;-->
-    <!--        <div>-->
-    <!--          <div>-->
-    <!--            <p>Om betalingen via Timerent te laten verlopen vragen we je om een aantal stappen te voltooien bij onze-->
-    <!--              partner Stripe.</p>-->
-    <!--            <br>-->
-    <!--            <p class="font-semibold">Belangrijk is dat je je persoonlijke- en bedrijfsgegevens bij de hand houd en de-->
-    <!--              verificatie in één keer afmaakt.</p>-->
-    <!--          </div>-->
-    <!--          <button @click="setupTimerentPayments" class="btn text-white bg-green-500 my-12"><i v-if="loading"-->
-    <!--                                                                                              class="fa fa-spinner mr-2 animate-spin"></i>-->
-    <!--            Start verificatie-->
-    <!--          </button>-->
-    <!--        </div>-->
-    <!--      </div>-->
-    <!--      <div v-else-if="venue.stripe_connect_id && venue.stripe_connect_onboarded">-->
-    <!--        Je account is onboarded voor Timerent Payments en je kunt direct betalingen ontvangen.-->
-    <!--      </div>-->
-    <!--    </div>-->
-
     <Modal v-if="openPaymentsSetupModal">
       <div class="p-3 space-y-3">
         <h1 class="font-semibold">Rond je verificatie af</h1>

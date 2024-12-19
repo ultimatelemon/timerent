1ddde2deee22228965cc29ad9f193a12fc93340c
ItsJusty <k.terpstra2000@icloud.com>
2024-12-19 22:55:04 +0100

    Front end fix

--- resources/js/pages/Components/Alerts/SuccessAlert.vue
+++ resources/js/pages/Components/Alerts/SuccessAlert.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="rounded-md bg-green-50 p-4 my-12">
+  <div class="rounded-md bg-green-50 dark:bg-green-400 p-4 my-12">
     <div class="flex items-center">
       <div class="flex-shrink-0">
-        <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <svg class="h-5 w-5 text-green-400 dark:text-green-800" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
           <path fill-rule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
                 clip-rule="evenodd"/>
         </svg>
       </div>
       <div class="ml-3 flex justify-between w-full items-center">
-        <div class="text-sm font-medium text-green-800">
+        <div class="text-sm font-medium text-green-800 dark:text-green-900">
           <slot></slot>
         </div>
         <button @click="this.close" type="button"
-                class="rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
+                class="rounded-md bg-green-50 dark:bg-green-700 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50">
           <span class="sr-only">Dismiss</span>
           <svg class="h-5 w-5 flex" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path
--- resources/js/pages/Venues/Settings/Finance.vue
+++ resources/js/pages/Venues/Settings/Finance.vue
@@ -5,10 +5,9 @@
       <div class="text-sm dark:text-slate-400">Hoe wil je betalingen ontvangen?</div>
     </div>
 
-    <div v-if="success" class="my-12 alert alert-success">
+    <SuccessAlert v-if="success" @close="success = false">
       <div>Je instellingen zijn opgeslagen</div>
-      <i class="fa fa-close cursor-pointer" @click="success = false"></i>
-    </div>
+    </SuccessAlert>
 
     <div class="my-12">
       <div class="flex flex-wrap gap-2 justify-items-stretch">
@@ -30,51 +29,97 @@
       </div>
     </div>
 
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">
-      <div v-if="formData.payment_service_provider !== 'timerent'">
-        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">API key<span
-            v-if="true"
-            class="required-star">*</span> <span class="text-xs dark:text-slate-400">(Je API key is vanwege veiligheid niet meer zichtbaar na het opslaan van je instellingen)</span></label>
-        <div class="relative mt-2 rounded-md shadow-sm">
-          <input v-model="formData.payment_api_key" type="text" name="name" id="name"
-                 v-on:keyup.enter="postData"
-                 :class="errors.name ? 'ring-red-300' : ''"
-                 class="block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 input"
-                 aria-invalid="true" aria-describedby="name-error"/>
-          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
-            <svg v-if="errors.name" class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor"
-                 aria-hidden="true">
-              <path fill-rule="evenodd"
-                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"
-                    clip-rule="evenodd"/>
-            </svg>
-          </div>
-        </div>
+    <div v-if="formData.payment_service_provider === 'timerent'">
+      <div v-if="venue.stripe_connect_id && venue.stripe_connect_onboarded" class="text-green-500">
+        <i class="fa fa-check"></i> Je kunt direct gebruik maken van Timerent Payments
       </div>
       <div v-else>
-        <!--        <div v-if="venue.stripe_connect_id && venue.stripe_connect_onboarded" class="text-green-500"><i class="fa fa-check"></i> Je kunt gebruik maken van Timerent Payments</div>-->
-        <div v-if="!venue.stripe_connect_id || !venue.stripe_connect_onboarded && !venue.payment_service_provider">
-          <div>
-            <p>Om betalingen via Timerent te laten verlopen vragen we je om een aantal stappen te voltooien bij onze
-              partner Stripe.</p>
-            <br>
-            <p class="font-semibold">Belangrijk is dat je je persoonlijke- en bedrijfsgegevens bij de hand houd en de
-              verificatie in één keer afmaakt.</p>
-          </div>
-          <button @click="setupTimerentPayments" class="btn text-white bg-green-500 my-12"><i v-if="loading"
-                                                                                              class="fa fa-spinner mr-2 animate-spin"></i>
-            Start verificatie
-          </button>
+        <div>
+          <p>Om betalingen via Timerent te laten verlopen vragen we je om een aantal stappen te voltooien bij onze
+            partner Stripe.</p>
+          <br>
+          <p class="font-semibold">Belangrijk is dat je je persoonlijke- en bedrijfsgegevens bij de hand houd en de
+            verificatie in één keer afmaakt.</p>
+        </div>
+        <button @click="setupTimerentPayments" class="btn text-white bg-green-500 my-12"><i v-if="loading"
+                                                                                            class="fa fa-spinner mr-2 animate-spin"></i>
+          Start verificatie
+        </button>
+      </div>
+    </div>
+    <div v-else-if="formData.payment_service_provider === 'mollie'">
+      <label for="name" class="block text-sm font-medium leading-6 text-gray-900">API key
+        <span
+            v-if="true"
+            class="required-star">*</span> <span class="text-xs dark:text-slate-400">(Je API key is vanwege veiligheid niet meer zichtbaar na het opslaan van je instellingen)</span>
+      </label>
+      <div class="relative mt-2 rounded-md shadow-sm">
+        <input v-model="formData.payment_api_key" type="text" name="name" id="name"
+               v-on:keyup.enter="postData"
+               :class="errors.name ? 'ring-red-300' : ''"
+               class="block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 input"
+               aria-invalid="true" aria-describedby="name-error"/>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+          <svg v-if="errors.name" class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor"
+               aria-hidden="true">
+            <path fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"
+                  clip-rule="evenodd"/>
+          </svg>
         </div>
       </div>
     </div>
+
+    <!--    <div class="grid grid-cols-1 lg:grid-cols-2 gap-12">-->
+    <!--      <div v-if="formData.payment_service_provider !== 'timerent'">-->
+    <!--        <label for="name" class="block text-sm font-medium leading-6 text-gray-900">API key<span-->
+    <!--            v-if="true"-->
+    <!--            class="required-star">*</span> <span class="text-xs dark:text-slate-400">(Je API key is vanwege veiligheid niet meer zichtbaar na het opslaan van je instellingen)</span></label>-->
+    <!--        <div class="relative mt-2 rounded-md shadow-sm">-->
+    <!--          <input v-model="formData.payment_api_key" type="text" name="name" id="name"-->
+    <!--                 v-on:keyup.enter="postData"-->
+    <!--                 :class="errors.name ? 'ring-red-300' : ''"-->
+    <!--                 class="block w-full rounded-md border-0 py-1.5 pr-10 ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6 input"-->
+    <!--                 aria-invalid="true" aria-describedby="name-error"/>-->
+    <!--          <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">-->
+    <!--            <svg v-if="errors.name" class="h-5 w-5 text-red-500" viewBox="0 0 20 20" fill="currentColor"-->
+    <!--                 aria-hidden="true">-->
+    <!--              <path fill-rule="evenodd"-->
+    <!--                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0v-4.5A.75.75 0 0110 5zm0 10a1 1 0 100-2 1 1 0 000 2z"-->
+    <!--                    clip-rule="evenodd"/>-->
+    <!--            </svg>-->
+    <!--          </div>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--      <div v-else-if="!venue.stripe_connect_id || !venue.stripe_connect_onboarded">-->
+    <!--        &lt;!&ndash;        <div v-if="venue.stripe_connect_id && venue.stripe_connect_onboarded" class="text-green-500"><i class="fa fa-check"></i> Je kunt gebruik maken van Timerent Payments</div>&ndash;&gt;-->
+    <!--        <div>-->
+    <!--          <div>-->
+    <!--            <p>Om betalingen via Timerent te laten verlopen vragen we je om een aantal stappen te voltooien bij onze-->
+    <!--              partner Stripe.</p>-->
+    <!--            <br>-->
+    <!--            <p class="font-semibold">Belangrijk is dat je je persoonlijke- en bedrijfsgegevens bij de hand houd en de-->
+    <!--              verificatie in één keer afmaakt.</p>-->
+    <!--          </div>-->
+    <!--          <button @click="setupTimerentPayments" class="btn text-white bg-green-500 my-12"><i v-if="loading"-->
+    <!--                                                                                              class="fa fa-spinner mr-2 animate-spin"></i>-->
+    <!--            Start verificatie-->
+    <!--          </button>-->
+    <!--        </div>-->
+    <!--      </div>-->
+    <!--      <div v-else-if="venue.stripe_connect_id && venue.stripe_connect_onboarded">-->
+    <!--        Je account is onboarded voor Timerent Payments en je kunt direct betalingen ontvangen.-->
+    <!--      </div>-->
+    <!--    </div>-->
 
     <Modal v-if="openPaymentsSetupModal">
       <div class="p-3 space-y-3">
         <h1 class="font-semibold">Rond je verificatie af</h1>
         <p class="text-sm">Klik op de knop om je verificatie in 1 keer af te ronden.</p>
         <div class="flex justify-end">
-          <a :href="paymentsSetupURL" @click="openPaymentsSetupModal = false; paymentsSetupURL = null;" target="_blank" class="btn btn-primary">Afronden</a>
+          <a :href="paymentsSetupURL" @click="openPaymentsSetupModal = false; paymentsSetupURL = null;"
+             target="_blank"
+             class="btn btn-primary">Afronden</a>
         </div>
       </div>
     </Modal>
@@ -91,10 +136,11 @@
 <script>
 
 import Modal from "../../Components/Modals/Modal.vue";
+import SuccessAlert from "../../Components/Alerts/SuccessAlert.vue";
 
 export default {
   name: "Finance",
-  components: {Modal},
+  components: {SuccessAlert, Modal},
   data() {
     return {
 
@@ -126,6 +172,7 @@ export default {
           .then(response => {
             this.success = true;
             this.errors = [];
+            this.fetchData();
           })
           .catch(e => {
             this.errors = e.response.data.errors;
@@ -137,7 +184,7 @@ export default {
     },
 
     setupTimerentPayments() {
-      if(this.loading) return;
+      if (this.loading) return;
       this.loading = true;
 
       axios.post('/venue/' + this.$route.params.venue + '/payments/setup')
